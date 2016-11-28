ee919fad5c29613f948a18cb3d9a952013a80aa1
Silvan Egli <siegli@student.ethz.ch>
2016-11-28 21:49:19 +0100

    bugfix

--- backend/django/chat/views.py
+++ backend/django/chat/views.py
@@ -1,7 +1,4 @@
-from rest_framework.decorators import permission_classes, api_view
 from rest_framework.generics import ListCreateAPIView
-from rest_framework.permissions import IsAdminUser
-from rest_framework.response import Response
 
 from chat.serializers import MessageSerializer
 from chat.models import Message
@@ -17,7 +14,7 @@ class RetrieveCreateMessages(ListCreateAPIView):
         user = self.request.user
 
         #if user is admin check message for special command codewords and execute command if any
-        if user.is_superuser():
+        if user.is_superuser:
             execute_admin_command(message_text)
 
         message = Message.objects.create(text=message_text, writer=user)
