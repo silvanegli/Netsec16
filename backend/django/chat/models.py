1ea9236c3bdbc1b4e56843324c7dcc7340cfa2d9
Silvan Egli <siegli@student.ethz.ch>
2016-11-28 09:04:26 +0100

    backend bootstrap

---
+++ backend/django/chat/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Message(models.Model):
+    text = models.CharField(max_length=600, name='text')
+    writer = models.ForeignKey(User, name='writer')
+    created_at = models.DateTimeField('created at', auto_now_add=True)
