9b4dfda8b33150e41e6042b7ef2950e8ffaf8a91
Lukas Bischofberger <me@worx.li>
2016-11-30 17:08:51 +0100

    added admin color

--- frontend/src/app/api/api.config.ts
+++ frontend/src/app/api/api.config.ts
@@ -1,4 +1,5 @@
-export const API_BASE_URL: string = 'http://localhost:8000/api/';
+export const API_BASE_URL: string = 'https://web.netchat/api/';
+// export const API_BASE_URL: string = 'http://localhost:8000/api/';
 
 export const LOGIN_ENDPOINT: string = 'login/';
 export const MESSAGE_ENDPOINT: string = 'messages/';
